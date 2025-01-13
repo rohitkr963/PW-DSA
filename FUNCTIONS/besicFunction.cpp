59721c9e73ca276bfe99bcbf17576a76ec647ac5
Rohit Kashyap <nk7565331@gmail.com>
2025-01-13 21:46:43 +0530

    besic of function

---
+++ FUNCTIONS/besicFunction.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+void greeting(){
+    cout<<"HELLO"<<endl;
+    cout<<"How are you"<<endl;
+}
+
+int main(){
+    greeting();
+    greeting();
+    greeting();
+
+    return 0;
+}
