37aca026f4d1724f828ab036a452a44919f63848
Rohit Kashyap <nk7565331@gmail.com>
2025-01-16 22:19:13 +0530

    null pointer

---
+++ POINTER/doublePointer.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    int x = 5;
+    int* ptr = &x;
+    int **p = &ptr;
+
+    cout<<x<<endl;      // value
+    cout<<*ptr<<endl;  // value
+    cout<<**p<<endl;  // value
+    cout<<p<<endl;   // address of ptr
+
+
+
+
+}
