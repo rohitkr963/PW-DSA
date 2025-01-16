47ad9512d8269a431f133c832af466f0f216f9be
Rohit Kashyap <nk7565331@gmail.com>
2025-01-16 22:12:09 +0530

    null pointer

--- POINTER/nullPointer.cpp
+++ POINTER/nullPointer.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main(){
     
-    int* ptr =  NULL;   //reserved address
-   cout<<ptr<<endl;    //0
+    int* ptr1 =  NULL;   //reserved address
+    int* ptr2=  0;   //reserved address
+    int* ptr3 =  '\0';   //reserved address
+
+   cout<<ptr1<<endl;    //0
+   cout<<ptr2<<endl;    //0
+   cout<<ptr3<<endl;    //0
+
 }
