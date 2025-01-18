f4f2fb56d8c717a03931a85d2906f03ba6c005d7
Rohit Kashyap <nk7565331@gmail.com>
2025-01-18 21:38:50 +0530

    print index

---
+++ ARRAY/printIndex.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {23,40,44,21,33};
+
+    for(int i = 0; i<=4; i++){
+        if(arr[i]<35){
+            cout<<i;
+        }
+    }
+}
