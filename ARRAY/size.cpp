e61c22a19e8898ca9e6d820603c836fcccc40510
Rohit Kashyap <nk7565331@gmail.com>
2025-01-20 21:14:21 +0530

    size of array

---
+++ ARRAY/size.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {23,40,44,21,33,4,3,5,7,8,3,2,1,55,44,23,43,21,421};
+    int length = sizeof(arr)/sizeof(arr[0]);
+    cout<<length<<endl;
+}
+
