0c09f25f7efc76b6763db5fce0adf6b339324b28
Rohit Kashyap <nk7565331@gmail.com>
2025-01-22 09:35:49 +0530

    linear Search

--- ARRAY/linearSearch.cpp
+++ ARRAY/linearSearch.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 int main(){
     int n,target;
+    cout<<"enter the size of array";
     cin>>n;
     int arr[n];
     for(int i = 0; i<n; i++){
         cin>>arr[i];
     }
 
-    cout<<"find the target value";
+    cout<<"find the target value : ";
     cin>>target;
    
+
+   bool flag = false;   
     for(int i = 0; i<n; i++){
-        if(arr[i] == target ){
-            cout<<"yes available at index = "<<i<<endl;
-        }
+        if(arr[i] == target )  flag = true;
     }
+    if(flag == true) cout<<"yes available"<<endl;
+    else cout<<"not available";
 }
