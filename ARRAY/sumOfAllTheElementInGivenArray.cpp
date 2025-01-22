9cf4669e307cb4350b2095ce1b58e8022f1a7f13
Rohit Kashyap <nk7565331@gmail.com>
2025-01-22 09:11:29 +0530

    sum of all the element given in array

---
+++ ARRAY/sumOfAllTheElementInGivenArray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    int arr[8] = {4,3,5,7,8,3,2,1};
+    for(int i = 0; i<=8; i++){
+        sum += i;
+    
+    }
+    cout<<sum<<endl;
+}
